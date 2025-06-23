e26dd55ed5420fc3023beb8ff70e27d782532b45
leyk <ms7939624@gmail.com>
2025-06-23 18:26:13 +0300

    add json tegs in post model

--- internal/models/post.go
+++ internal/models/post.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Post struct {
-	ID        int       `db:"id"`
-	Title     string    `db:"title"`
-	Content   string    `db:"content"`
-	CreatedAt time.Time `db:"created_at"`
+    ID        int       `db:"id" json:"id"`
+    Title     string    `db:"title" json:"title"`
+    Content   string    `db:"content" json:"content"`
+    CreatedAt time.Time `db:"created_at" json:"created_at"`
 }
